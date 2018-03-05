29991b276616e140be72a87364eef61a431271dc
1174062992 <1174062992>
2018-03-05 22:10:31 +0800

    Initial Commit

---
+++ 蓝牙中心设备/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  蓝牙中心设备
+//
+//  Created by 曹记 on 2018/3/5.
+//  Copyright © 2018年 曹记. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 蓝牙中心设备/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  蓝牙中心设备
+//
+//  Created by 曹记 on 2018/3/5.
+//  Copyright © 2018年 曹记. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
